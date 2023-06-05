f03244673e2d8988866da7f42b04dc860776a45b
lalithasaga <132065591+lalithasaga@users.noreply.github.com>
2023-06-05 21:16:04 +0530

    Add files via upload

---
+++ Expenseitem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+    <div>
+    <div>Expense Items</div>
+    <div>
+    <h2>cost</h2>
+    <div>Food items RS 10</div>
+    <div>Petrol RS 100</div>
+    <div>Movies RS 200</div>
+    </div>
+    </div>);
+}
+export default ExpenseItem;
